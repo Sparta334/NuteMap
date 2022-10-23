c85bf1fce6db6d0da1ed17a60432e20bd898088a
Spartan334 <ianmina0322@gmail.com>
2022-10-23 15:00:47 +0800

    再次修正

--- js/left-area.js
+++ js/left-area.js
@@ -6,7 +6,7 @@ StoreContainer.addEventListener("wheel" , function(event){
     event.preventDefault;
 
     for(let i = 0 ; i<1 ; i+=0.1 )
-        StoreContainer.scrollLeft += event.deltaY*i*7;
+        StoreContainer.scrollLeft += event.deltaY*i*6;
   
 
 });

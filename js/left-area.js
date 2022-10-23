8e4f1c52d5dabec0c969b7aab824ed4f9c646f58
Spartan334 <ianmina0322@gmail.com>
2022-10-23 18:41:32 +0800

    RWD

--- js/left-area.js
+++ js/left-area.js
@@ -11,3 +11,17 @@ StoreContainer.addEventListener("wheel" , function(event){
 
 });
 
+
+if(document.documentElement.clientHeight >= document.documentElement.clientWidth ){
+
+    let HeaderIMG = document.getElementsByClassName('HeaderImg');
+    for(let i = 0 ; i< HeaderIMG.length ; i++){
+        HeaderIMG[i].style.height = "7vw";
+    }
+    document.querySelector('.Header').style.height ="8vw";
+    
+    document.querySelector('.Home').style.height ="3vw";
+    document.querySelector('.Author').style.height ="3vw";
+
+}
+
